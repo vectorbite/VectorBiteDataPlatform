38b6f280056da491f85e7f6a4938f9cd926ef569
Francis Windram <francis.windram17@imperial.ac.uk>
2019-05-10 10:57:34 +0100

    Add per-subsite navbars

--- models/menu.py
+++ models/menu.py
@@ -4,33 +4,58 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # this is the main application menu add/remove items as required
 # ----------------------------------------------------------------------------------------------------------------------
-if auth.is_logged_in():
-    response.menu = [
-        (T('Home'), False, URL('default', 'index'), []),
-        ('Get Data', False, None, [
-            ('VecDyn', False, URL('vecdyn_query_1', 'vecdyn_taxon_location_query'))]),
-        ('Submit Data', False, None,[
-            ('Submit VecDyn Dataset', False, URL('default', 'submit_vecdyn_data'))]),
-         (T('Contact Us'), False, None,[
-                ('Report problem', False, URL('default', 'report_problem')),
-                ('General enquiries', False, URL('default', 'contact_us'))]),
-             #(T('About Us'), False, URL('default', 'about_us'))]),
 
+response.menu = [
+    (T('Home'), False, URL('default', 'index')),
 ]
 
 
+if request.controller == 'vectraits':
+    # VT-specific navbar
+    response.menu.append(('VecTraits', False, URL('vectraits', 'index')))
+    if auth.is_logged_in():
+        response.menu.append(
+            (T('Database'), False, None, [
+                ('Submit', False, URL('vectraits', 'index')),   # TODO: change to submit link
+                ('Explore', False, URL('vectraits', 'view_vectraits')),
+                ('Validate', False, URL('vectraits', 'validate_test')),
+            ])
+        )
+    response.menu.append(('About VecTraits', False, URL('vectraits', 'about')))
+elif request.controller in ('vecdyn', 'vecdyn_data_uploader', 'vecdyn_query_1', 'vecdyn_query_2'):
+    # VD-specific navbar
+    response.menu.append(('VecDyn', False, URL('vecdyn', 'index')))
+    if auth.is_logged_in():
+        response.menu.append(
+            (T('Database'), False, None, [
+                ('Submit', False, URL('default', 'submit_vecdyn_data')),  # TODO: move to vecdyn controller
+                ('Explore', False, URL('vecdyn_query_1', 'vecdyn_taxon_location_query')),
+            ])
+        )
+    response.menu.append(('About VecDyn', False, URL('vecdyn', 'about')))
+else:
+    # Default/all else navbar
+    response.menu.extend([
+        (T('Databases'), False, None, [
+            ('VecDyn', False, URL('vecdyn', 'index')),
+            ('VecTraits', False, URL('vectraits', 'index')),
+        ]),
+        (T('About Us'), False, URL('default', 'about_us')),
+        (T('Contact Us'), False, None, [
+            ('Report problem', False, URL('default', 'report_problem')),
+            ('General enquiries', False, URL('default', 'contact_us'))]),
+    ])
 
-#if auth.has_membership('VectorbiteAdmin'):
- #   response.menu.append((T('Manage Data Sets'), False, None,[
-  #              ('VecDyn data collections', False, URL('vecdyn', 'dataset_registrations'))]))
 
 if auth.has_membership('VectorbiteAdmin'):
-    response.menu.append((T('Management'), False, None,[
-                ('Manage VecDyn Datasets', False, URL('vecdyn', 'dataset_registrations')),
-                ('Task Manager', False, URL('default', 'tasks')),
-                ('Group Membership', False, URL('default', 'group_membership'))]))
+    # Admin navbar
+    response.menu.append((T('Admin'), False, None, [
+        ('Manage VecDyn Datasets', False, URL('vecdyn', 'dataset_registrations')),
+        ('Manage VecTraits Datasets', False, URL('vectraits', 'index')),    # TODO: Change to management interface when created
+        ('Task Manager', False, URL('default', 'tasks')),
+        ('Group Membership', False, URL('default', 'group_membership'))]))
 
-
+# TODO: Work out whether the below is still needed
 #if auth.has_membership('VectorbiteManagers'):
  #   response.menu.append((T('Management'), False, None,[
                 #('Manage Collections', False, URL('vecdyn', 'manage_collections')),
