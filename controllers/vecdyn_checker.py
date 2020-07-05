99b775a5e49f360832a8d333e71a962cdf57b5f0
Matthew Watts <m.watts@imperial.ac.uk>
2020-07-05 16:35:59 +0200

    vecdyn validator WIP working

--- controllers/vecdyn_checker.py
+++ controllers/vecdyn_checker.py
@@ -19,7 +19,7 @@ def vecdyn_csv_checker():
     form = SQLFORM(db.vecdyn_validator, comments=False, fields=['csvfile'],
                    labels={'csvfile': 'Click to search and select a file:'})
     if form.process().accepted:
-        session.flash = 'Validating, please wait'
+        #session.flash = 'Validating, please wait'
         filename, csvfile = db.vecdyn_validator.csvfile.retrieve(form.vars.csvfile)
         csvfile = csv.reader(csvfile)
         # Get all rows of csv from csv_reader object as list of tuples
@@ -27,95 +27,134 @@ def vecdyn_csv_checker():
         # a simple validator to be tested
         validator = CSVValidator(vecdyn_field_names)
 
-        validator.add_value_check('taxon',string_required(str),
-                                  'taxon error', 'Taxon entry must be a string')
-        # validator.add_value_check('location_description', str,
-        #                           'location_description error', 'entry must be a string')
-        # validator.add_value_check('study_collection_area', datatype_not_required(float),
-        #                            'study_collection_area', 'entry must be a string')
-        # validator.add_value_check('geo_datum', datatype_not_required(str),
-        #                           'geo_datum error', 'entry must be a string')
-        # validator.add_value_check('gps_obfuscation_info', datatype_not_required(str),
-        #                           'gps_obfuscation_info', 'entry must be a string')
-        # validator.add_value_check('species_id_method', datatype_not_required(str),
-        #                           'species_id_method', 'entry must be a string')
-        # validator.add_value_check('study_design', datatype_not_required(str),
-        #                           'study_design', 'entry must be a string')
-        # validator.add_value_check('sampling_strategy', datatype_not_required(str),
-        #                           'sampling_strategy', 'entry must be a string')
-        # validator.add_value_check('sampling_method', datatype_not_required(str),
-        #                           'sampling_method', 'entry must be a string')
-        # validator.add_value_check('sampling_protocol', datatype_not_required(str),
-        #                           'sampling_protocol', 'entry must be a string')
-        # validator.add_value_check('sampling_method', datatype_not_required(str),
-        #                           'sampling_method', 'entry must be a string')
-        # validator.add_value_check('measurement_unit', datatype_not_required(str),
-        #                           'measurement_unit', 'entry must be a string')
-        # validator.add_value_check('sample_start_date', datetime_string('%Y-%m-%d'),
-        #                           'value_transform', 'entry must be a string')
-        # validator.add_value_check('sample_start_time', datetime_string('%hh:%mm:%ss'),
-        #                           'sampling_method', 'entry must be a string')
-        # validator.add_value_check('taxon', datatype_not_required(),
-        #                           'foo', 'not an integer')
-        # validator.add_value_check('bar', not_required_datatype(float),
-        #                           'bar', 'not a float')
+        # basic header and record length checks
+        validator.add_header_check('dataset_check', 'bad header')
 
+        validator.add_value_check('taxon', string_required(str),
+                                  'taxon error', 'entry must be a string')
+        validator.add_value_check('location_description', string_required(str),
+                                   'location_description error', 'entry must be a string')
+        validator.add_value_check('study_collection_area', datatype_not_required(str),
+                                    'study_collection_area', 'entry must be a string')
+        validator.add_value_check('geo_datum', datatype_not_required(str),
+                                   'geo_datum error', 'entry must be a string')
+        validator.add_value_check('gps_obfuscation_info', datatype_not_required(str),
+                                   'gps_obfuscation_info', 'entry must be a string')
+        validator.add_value_check('species_id_method', datatype_not_required(str),
+                                   'species_id_method', 'entry must be a string')
+        validator.add_value_check('study_design', datatype_not_required(str),
+                                   'study_design', 'entry must be a string')
+        validator.add_value_check('sampling_strategy', datatype_not_required(str),
+                                   'sampling_strategy', 'entry must be a string')
+        validator.add_value_check('sampling_method', datatype_not_required(str),
+                                   'sampling_method', 'entry must be a string')
+        validator.add_value_check('sampling_protocol', datatype_not_required(str),
+                                   'sampling_protocol', 'entry must be a string')
+        validator.add_value_check('sampling_method', datatype_not_required(str),
+                                   'sampling_method', 'entry must be a string')
+        validator.add_value_check('measurement_unit', datatype_not_required(str),
+                                   'measurement_unit', 'entry must be a string')
+        validator.add_value_check('sample_start_date', datetime_string('%Y-%m-%d'),
+                                   'value_transform', 'entry must be in date format: %Y-%m-%d')
+        validator.add_value_check('sample_start_time', time_string_not_required('%H:%M:%S'),
+                                   'sampling_method', 'entry must be in time format: %H:%M:%S')
+        validator.add_value_check('sample_end_date', datetime_string_not_required('%Y-%m-%d'),
+                                   'value_transform', 'entry must be in date format: %Y-%m-%d')
+        validator.add_value_check('sample_end_time', datetime_string_not_required('%H:%M:%S'),
+                                    'sampling_method', 'entry must be a string')
+        validator.add_value_check('sample_value', float_or_int_required(),
+                                  'sample_value error', 'entry must be numeric')
+        validator.add_value_check('sample_sex', datatype_not_required(str),
+                                  'sample_sex error', 'entry must be a string')
+        validator.add_value_check('sample_stage', datatype_not_required(str),
+                                  'sample_stage error', 'entry must be a string')
+        validator.add_value_check('sample_location', datatype_not_required(str),
+                                  'sample_location error', 'entry must be a string')
+        validator.add_value_check('sample_collection_area', datatype_not_required(str),
+                                  'sample_collection_area', 'entry must be a string')
+        validator.add_value_check('sample_lat_dd', float,
+                                  'sample_lat_dd error', 'sample_long_dd must be a float or not empty')
+        validator.add_value_check('sample_long_dd', float,
+                                  'sample_long_dd', 'sample_long_dd must be a float or not empty')
+        validator.add_value_check('sample_environment', datatype_not_required(str),
+                                  'sample_environment', 'entry must be a string')
+        validator.add_value_check('additional_location_info', datatype_not_required(str),
+                                  'additional_location_info', 'entry must be a string')
+        validator.add_value_check('additional_sample_info', datatype_not_required(str),
+                                  'additional_sample_info', 'entry must be a string')
+        validator.add_value_check('sample_name', datatype_not_required(str),
+                                  'sample_name', 'entry must be a string')
 
         summarize = False
         limit = 0
         # some test data
-        problems = validator.validate(csvfile)
-        """
-            Write problems as restructured text to a file (or stdout/stderr).
+        problems = validator.ivalidate(csvfile)
+        if problems == "":
+            return "no problems"
+        else:
+            #return problems
             """
-        #file = open('myfile.txt', 'w')
-        file = cStringIO.StringIO()
-        w = file.write  # convenience variable
-        w("""
-        =================
-        Vecdyn Validation Report
-        =================
-        """)
-        counts = dict()  # store problem counts per problem code
-        total = 0
-        for i, p in enumerate(problems):
-            if limit and i >= limit:
-                break  # bail out
-            if total == 0 and not summarize:
-                w("""
-        Problems
-        ========
-        """)
-            total += 1
-            code = p['code']
-            if code in counts:
-                counts[code] += 1
-            else:
-                counts[code] = 1
-            if not summarize:
-                ptitle = '\n%s - %s\n' % (p['code'], p['message'])
-                w(ptitle)
-                underline = ''
-                for i in range(len(ptitle.strip())):
-                    underline += '-'
-                underline += '\n'
-                w(underline)
-                for k in sorted(p.viewkeys() - set(['code', 'message', 'context'])):
-                    w(':%s: %s\n' % (k, p[k]))
-                if 'context' in p:
-                    c = p['context']
-                    for k in sorted(c.viewkeys()):
-                        w(':%s: %s\n' % (k, c[k]))
+                Write problems as restructured text to a file (or stdout/stderr).
+                """
+            #file = open('myfile.txt', 'w')
+            file = cStringIO.StringIO()
+            w = file.write  # convenience variable
+            w("""
+            ========================
+            Vecdyn Validation Report
+            ========================
+            Satus = "Failed"
+            =================================================================
+            Please fix the problems below before resubmitting the dataset.
+            =================================================================
+            """)
+            counts = dict()  # store problem counts per problem code
+            total = 0
+            for i, p in enumerate(problems):
+                if limit and i >= limit:
+                    break  # bail out
+                if total == 0 and not summarize:
+                    w("""
+            Problems
+            ========
+            """)
+                total += 1
+                code = p['code']
+                if code in counts:
+                    counts[code] += 1
+                else:
+                    counts[code] = 1
+                if not summarize:
+                    ptitle = '\n%s - %s\n' % (p['code'], p['message'])
+                    w(ptitle)
+                    underline = ''
+                    for i in range(len(ptitle.strip())):
+                        underline += '-'
+                    underline += '\n'
+                    w(underline)
+                    for k in sorted(p.viewkeys() - set(['code', 'message', 'context'])):
+                        w(':%s: %s\n' % (k, p[k]))
+                    if 'context' in p:
+                        c = p['context']
+                        for k in sorted(c.viewkeys()):
+                            w(':%s: %s\n' % (k, c[k]))
 
-        w("""
-        Summary
-        =======
-        Found %s%s problem%s in total.
-        """ % ('at least ' if limit else '', total, 's' if total != 1 else ''))
-        for code in sorted(counts.viewkeys()):
-            w(':%s: %s\n' % (code, counts[code]))
-        file.seek(0)  # <---
-        #return file
-        return response.stream(file, attachment=True, filename='Vecdyn-Validation-Report.txt')
+            w("""
+            Summary
+            =======
+            Found %s%s problem%s in total.
+            """ % ('at least ' if limit else '', total, 's' if total != 1 else ''))
+            for code in sorted(counts.viewkeys()):
+                w(':%s: %s\n' % (code, counts[code]))
+            if total == 0:
+                return 'no probs'
+            else:
+                file.seek(0)  # <---
+                response.headers['Content-Type'] = '.txt'
+                response.flash = "failed"
+                # delete dataset
+                return file.getvalue()
     return locals()
 
+def view_errors():
+    return locals()
--- modules/vecdyn_csv_validator.py
+++ modules/vecdyn_csv_validator.py
@@ -1,85 +1,8 @@
-"""
-
-This module provides some simple utilities for validating data contained in CSV
-files, or other similar data sources.
-
-Note that the `csvvalidator` module is intended to be used in combination with
-the standard Python `csv` module. The `csvvalidator` module **will not**
-validate the *syntax* of a CSV file. Rather, the `csvvalidator` module can be
-used to validate any source of row-oriented data, such as is provided by a
-`csv.reader` object.
-
-I.e., if you want to validate data from a CSV file, you have to first construct
-a CSV reader using the standard Python `csv` module, specifying the appropriate
-dialect, and then pass the CSV reader as the source of data to either the
-`CSVValidator.validate` or the `CSVValidator.ivalidate` method.
-
-The `CSVValidator` class is the foundation for all validator objects that are
-capable of validating CSV data.
-
-You can use the CSVValidator class to dynamically construct a validator, e.g.::
-
-    import sys
-    import csv
-    from csvvalidator import *
-
-    field_names = (
-                   'study_id',
-                   'patient_id',
-                   'gender',
-                   'age_years',
-                   'age_months',
-                   'date_inclusion'
-                   )
-
-    validator = CSVValidator(field_names)
-
-    # basic header and record length checks
-    validator.add_header_check('EX1', 'bad header')
-    validator.add_record_length_check('EX2', 'unexpected record length')
-
-    # some simple value checks
-    validator.add_value_check('study_id', int,
-                              'EX3', 'study id must be an integer')
-    validator.add_value_check('patient_id', int,
-                              'EX4', 'patient id must be an integer')
-    validator.add_value_check('gender', enumeration('M', 'F'),
-                              'EX5', 'invalid gender')
-    validator.add_value_check('age_years', number_range_inclusive(0, 120, int),
-                              'EX6', 'invalid age in years')
-    validator.add_value_check('date_inclusion', datetime_string('%Y-%m-%d'),
-                              'EX7', 'invalid date')
-
-    # a more complicated record check
-    def check_age_variables(r):
-        age_years = int(r['age_years'])
-        age_months = int(r['age_months'])
-        valid = (age_months >= age_years * 12 and
-                 age_months % age_years < 12)
-        if not valid:
-            raise RecordError('EX8', 'invalid age variables')
-    validator.add_record_check(check_age_variables)
-
-    # validate the data and write problems to stdout
-    data = csv.reader('/path/to/data.csv', delimiter='\t')
-    problems = validator.validate(data)
-    write_problems(problems, sys.stdout)
-
-For more complex use cases you can also sub-class `CSVValidator` to define
-re-usable validator classes for specific data sources.
-
-The source code for this module lives at:
-
-    https://github.com/alimanfoo/csvvalidator
-
-For a complete account of all of the functionality available from this module,
-see the example.py and tests.py modules in the source code repository.
-
-"""
-
 
 import re
 from datetime import datetime
+import time
+
 
 
 UNEXPECTED_EXCEPTION = 0
@@ -997,6 +920,14 @@ def number_range_exclusive(min, max, type=float):
             raise ValueError(v)
     return checker
 
+def float_or_int_required(type1=int, type2=float):
+    """
+    Return a value check function which raises a ValueError if the supplied
+    value is not a decimal or integer.
+    """
+    def checker(v):
+        type1(v) or type2(v)
+    return checker
 
 def datetime_string(format):
     """
@@ -1006,12 +937,11 @@ def datetime_string(format):
     See also `datetime.strptime`.
 
     """
-
     def checker(v):
         datetime.strptime(v, format)
     return checker
 
-def time_string(format):
+def datetime_string_not_required(format):
     """
     Return a value check function which raises a ValueError if the supplied
     value cannot be converted to a datetime using the supplied format string.
@@ -1020,14 +950,28 @@ def time_string(format):
 
     """
     def checker(v):
-        if v is "":
-            v = v + "00:00:00"
-            datetime.time(v)
+        if v == '':
+            pass
         else:
-            datetime.time(v, format)
+            datetime.strptime(v, format)
     return checker
 
+def time_string_not_required(format):
+    """
+    Return a value check function which raises a ValueError if the supplied
+    value cannot be converted to a datetime using the supplied format string.
 
+    See also `datetime.strptime`.
+
+    """
+    def checker(v):
+        if v == '':
+            pass
+        else:
+            time.strptime(v, format)
+    return checker
+
+# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
 def datetime_range_inclusive(min, max, format):
     """
     Return a value check function which raises a ValueError if the supplied
@@ -1069,13 +1013,12 @@ def datatype_not_required(type):
     """
     def checker(v):
         if v is "":
-            v = v + "1"
-            type(v)
+            pass
         else:
             type(v)
     return checker
 
-def string_required():
+def string_required(type):
     """
     Return a value check function which raises a ValueError if string is empty
     """
